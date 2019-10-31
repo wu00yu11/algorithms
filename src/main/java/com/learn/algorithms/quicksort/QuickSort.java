8b19e37a3435db1faeaeb4b4422bce6a0476b277
wu00yu11 <wu00yu11@163.com>
2019-10-31 23:35:13 +0800

    1.修改包路径名 2.增加选择排序 3.希尔排序 4.堆排序 5.快速排序 6.归并排序

---
+++ src/main/java/com/learn/algorithms/quicksort/QuickSort.java
@@ -0,0 +1,64 @@
+package com.learn.algorithms.quicksort;
+
+import java.util.Arrays;
+
+/**
+ * <strong>思路:</strong>
+ *通过一趟排序，将要排序的数据分隔成独立的两部分，其中一部分的所有数据比另外一部分的所有数据都要小，
+ * 然后再按此方法对这两部分数据分别进行快速排序，整个排序过程可以递归进行，以此使整个数据变成有序序列。
+ * <strong>时间复杂度:</strong>
+ *最佳情况：T(n) = O(nlogn) 最差情况：T(n) = O(n2) 平均情况：T(n) = O(nlogn)　
+ * @author wu00y
+ */
+public class QuickSort {
+    /**
+     * 排序方法
+     * @param arr 要排序的数组
+     * @param low 小的一边
+     * @param high 大的一边
+     * @return 排序后的数组，toString 方便输出
+     */
+    public static String quickSort(int[] arr, int low, int high) {
+
+        if(low > high) {
+            return Arrays.toString(arr);
+        }
+
+        int tmp = arr[low];
+        int i = low;
+        int j = high;
+
+        while(i < j) {
+            //先看右边，依次往左递减，直到找到小于基准数的数字
+            while(arr[j] >= tmp && i < j) {
+                j--;
+            }
+            //比基准小的记录移到低端
+            arr[i] = arr[j];
+
+            //后看左边，依次往右递增，直到找到大于基准数的数字
+            while(arr[i] <= tmp && i < j) {
+                i++;
+            }
+            //比基准大的记录移到高端
+            arr[j] = arr[i];
+        }
+
+        //记录基准
+        arr[i] = tmp;
+
+        //递归调用左半数组
+        quickSort(arr, low, j-1);
+        //递归调用右半数组
+        quickSort(arr, j+1, high);
+
+        return Arrays.toString(arr);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {111, 3, 5, 52, 74, 312, 75, 3, 764, 3, 2111, 7, 31};
+
+        System.out.println("排序后的数组："+ quickSort(arr, 0, arr.length-1));
+    }
+}
---
+++ src/main/java/com/learn/algorithms/shellsort/ShellSort.java
@@ -0,0 +1,58 @@
+package com.learn.algorithms.shellsort;
+
+import java.util.Arrays;
+
+/**
+ *<strong>思路:</strong>
+ *希尔排序是希尔（Donald Shell）于1959年提出的一种排序算法。希尔排序也是一种插入排序，它是简单插入排序经过改进之后的一个更高效的版本，
+ * 也称为缩小增量排序，同时该算法是冲破O(n2）的第一批算法之一。它与插入排序的不同之处在于，它会优先比较距离较远的元素。希尔排序又叫缩小增量排序。
+ * 希尔排序是把记录按下表的一定增量分组，对每组使用直接插入排序算法排序；随着增量逐渐减少，每组包含的关键词越来越多，当增量减至1时，整个文件恰被
+ * 分成一组，算法便终止。
+ * <strong>时间复杂度</strong>
+ * 最佳情况：T(n) = O(nlog2 n) 最坏情况：T(n) = O(nlog2 n) 平均情况：T(n) =O(nlog2n)
+ *
+ * @author wu00y
+ */
+public class ShellSort {
+    /**
+     * 排序方法
+     * @param arr 待排序的数组
+     * @return toString 方便输出
+     */
+    public static String shellSort(int[] arr) {
+
+        //当前正在比较的数字（下方简称：“当前”）
+        int current;
+        //初始增量（此处为：希尔增量）
+        int gap = arr.length / 2;
+
+        //gap=1的时候，数组已经有序
+        while(gap > 0) {
+            for(int i=gap; i<arr.length; i++) {
+                current = arr[i];
+                //与“当前”同组的前一个数字
+                int preIndex = i - gap;
+
+                //找到同组内比“当前”小的数字
+                while(preIndex >= 0 && current < arr[preIndex]) {
+                    //在向后移动同组内已排好序的，大于“当前”的数字
+                    arr[preIndex + gap] = arr[preIndex];
+                    preIndex -= gap;
+                }
+                //插入“当前”到相应的位置
+                arr[preIndex + gap] = current;
+            }
+            //缩小增量
+            gap /= 2;
+        }
+
+
+        return Arrays.toString(arr);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {111, 3, 5, 52, 74, 312, 75, 3, 764, 3, 2111, 7, 31};
+        System.out.println("排序后的数组："+ shellSort(arr));
+    }
+}

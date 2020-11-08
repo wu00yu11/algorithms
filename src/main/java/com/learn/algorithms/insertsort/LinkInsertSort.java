b26043226e77bc8e666bddb2bf12dd7dcb1ccfa8
wu00yu@163.com <wu00yu@163.com>
2020-11-08 20:34:19 +0800

    字符串转数字

---
+++ src/main/java/com/learn/algorithms/insertsort/LinkInsertSort.java
@@ -0,0 +1,53 @@
+package com.learn.algorithms.insertsort;
+
+import sun.awt.image.ImageWatched;
+
+/**
+ * @author wu00y
+ */
+public class LinkInsertSort {
+    public static void main(String[] args) {
+
+    }
+    LinkNode sort(LinkNode head){
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        LinkNode cur = head;
+        LinkNode pre = null;
+        LinkNode next = head.next;
+        return next;
+    }
+
+    private class LinkNode {
+        public int val;
+        public LinkNode next;
+
+        public LinkNode(int val) {
+            this.val = val;
+        }
+    }
+
+    private class LinkList{
+        public LinkNode head;
+        public LinkNode tail;
+        private int size = 0;
+
+        void add(int val){
+            if (head == null && size == 0){
+                head = new LinkNode(val);
+                tail = head;
+            }else{
+                tail.next = new LinkNode(val);
+            }
+            ++size;
+        }
+
+        private void tailInsert(LinkNode node){
+            tail.next = node;
+            ++size;
+        }
+    }
+
+}
